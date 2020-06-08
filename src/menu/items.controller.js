c7dbea88b8629f73f3252a78b6a21c9b9cf94001
PoojithaBalanagu <51267967+PoojithaBalanagu@users.noreply.github.com>
2020-06-08 13:58:19 +0530

    Add files via upload

---
+++ src/menu/items.controller.js
@@ -0,0 +1,16 @@
+(function () {
+'use strict';
+
+// Items controller
+angular.module('MenuApp')
+.controller('ItemsController', ItemsController);
+
+ItemsController.$inject = ['items'];
+function ItemsController(items) {
+   console.log("Init ItemsController: ");
+   var mainlist = this;
+   mainlist.items = items.menu_items;
+   console.log("items: ",mainlist.items);
+}
+
+})();
